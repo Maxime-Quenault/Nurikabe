e5c0a5a22dc1607bc31fef1ad05e91548651b953
Joji-Stan <78601345+Joji-Stan@users.noreply.github.com>
2022-03-02 11:50:22 +0100

    Update Chronometre.rb

--- Chronometre.rb
+++ Chronometre.rb
@@ -1,3 +1,4 @@
+# Représente le Chrono du Nurikabe
 class Chronometre
 
     private_class_method :new
@@ -6,29 +7,35 @@ class Chronometre
     @temps
     # indiquant si le jeu est en pause
     @pause
-    # le sens du temps, s'il augmente ou diminue suivant le mode de jeu
-    @sens
 
-    attr_reader :temps, :pause, :sens
+    attr_reader :temps, :pause
 
-    # On définit notre propre façon de gérer un Chronomètre
-    def Chronometre.creer(unTemps, unSens)
-        new(unTemps, unSens)
+    # Constructeur du Chronomètre
+    def Chronometre.creer()
+        new()
     end
 
-    # la méthode initialize() gère le chronomètre suivant les critères du mode de jeu
-    def initialize(unTemps, unSens)
-        @temps = unTemps
+    # initialize le chronomètre avec un temps à 0
+    def initialize()
+        @temps = 0
         @pause = false
-        @sens = unSens 
 
         @debut = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-
     end 
 
-    #
+    # méthode incrémentant le temps total du chronometre
     def top()
+        if(!self.estEnPause?())
+            fin = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+            @temps = @temps + (@debut - fin)
+            @debut = fin 
+        end
 
+        if(@temps < 0)
+            @temps = 0
+        end
+
+        #return estNul?()
     end
 
     # renvoie si le temps est nul 
@@ -36,16 +43,22 @@ class Chronometre
         return @temps == 0 
     end
 
-    # 
+    # renvoie si le jeu en pause
+    def estEnPause?()
+        return @pause == true
+    end
+
+    # actionne le chronometre
     def demarre()
-        enlevePause()
-        
+        self.enlevePause()
+        @debut = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+        top()
     end
 
     # met en pause le jeu
     def metEnPause()
+        top()
         @pause = true 
-        # met en pause le temps 
     end
 
     # enleve la pause
@@ -53,9 +66,21 @@ class Chronometre
         @pause = false 
     end
 
-    # retourne le temps
+    # retourne le temps sous la forme float
     def getTemps()
+        top()
         return @temps
     end
 
+    # augmente le temps par un malus
+    def ajouteTemps(unMalus)
+        @temps += unMalus 
+    end 
+
+    # retourne le temps arrondi au dessous sous la forme de String
+=begin    def getTempsString()
+        top()
+        return format("%01d",(@temps.floor/60).to_s) + ":" + format("%02d",(@temps.floor%60).to_s)
+    end
+=end
 end
