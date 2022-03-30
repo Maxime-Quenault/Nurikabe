d49ee8f36024983148b33ecc1155f6f5d59db1c7
Maxime-Quenault <77842815+Maxime-Quenault@users.noreply.github.com>
2022-03-30 17:41:24 +0200

    modification du temps de base dans le mode de jeu Survie

--- Chrono/ChronometreSurvie.rb
+++ Chrono/ChronometreSurvie.rb
@@ -4,7 +4,7 @@ load "Chrono/Chronometre.rb"
 class ChronometreSurvie < Chronometre
 
     # Constante du temps de départ du Chronometre
-    CHRONOMETRE_BASE_TEMPS = 30
+    CHRONOMETRE_BASE_TEMPS = 300
 
     def ChronometreSurvie.creer()
         new()
