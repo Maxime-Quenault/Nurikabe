8846dd76d892e33d74bd8f8c3c23d6da7e83a4fb
Maxime-Quenault <77842815+Maxime-Quenault@users.noreply.github.com>
2022-03-31 19:06:11 +0200

    Add files via upload
    Sauvegarde de l'aventure pour chaque profil

--- Sauvegarde/Profil.rb
+++ Sauvegarde/Profil.rb
@@ -1,12 +1,24 @@
 load "Partie/Grille.rb"
+load "Aventure/Aventure.rb"
+
 class Profil
-    attr_accessor :pseudo, :parametre, :listePartieCommence, :imageJoueur
+    attr_accessor :pseudo, :parametre, :listePartieCommence, :imageJoueur, :uneAventureFacile, :uneAventureMoyen, :uneAventureDifficile
 
     def initialize(unPseudo)
         @imageJoueur = "Image/utilisateur.png"
         @pseudo = unPseudo
         @parametre = Parametre.new()
         @listePartieCommence = []
+
+        @uneAventureFacile = Aventure.creer(0)
+        @uneAventureFacile.generationAventure(10, 0)
+
+        @uneAventureMoyen = Aventure.creer(1)
+        @uneAventureMoyen.generationAventure(10, 1)
+
+        @uneAventureDifficile = Aventure.creer(2)
+        @uneAventureDifficile.generationAventure(10, 2)
+        
         if(!File.exist?("Sauvegarde/SauvegardeGrille/listeGrille#{@pseudo}.dump"))
             @listePartieCommence = Array.new
         else  
