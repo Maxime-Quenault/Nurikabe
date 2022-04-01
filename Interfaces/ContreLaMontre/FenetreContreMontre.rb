05a6ae7f9ef8b1e634364fae445b2a6eca9864ab
Maxime-Quenault <77842815+Maxime-Quenault@users.noreply.github.com>
2022-04-01 23:54:45 +0200

    recuperation du travail de Maxime
    commentaire complet du dossier Survie et du dossier Contre la Montre

--- Interfaces/ContreLaMontre/FenetreContreMontre.rb
+++ Interfaces/ContreLaMontre/FenetreContreMontre.rb
@@ -2,12 +2,34 @@ require 'gtk3'
 load "Interfaces/Fenetre.rb"
 load "Interfaces/ContreLaMontre/FenetreChoixGrilleCLM.rb"
 
+##
+# 	@author Quenault Maxime
+#
+#	Cette classe va permettre d'afficher le choix de difficulté du mode de jeu.
+#
+#	Voici les methodes de la classe FenetreContreMontre :
+#
+#	- initialize : cette methode est le constructeur, elle recupere le fichier glade et initialise ses VI.
+#	- gestionSignaux : permet d'attribuer des actions à tous les objets de l'interface récupéré dans le constructeur.
+#   - getObjet : permet de recuperer l'interface courante
+#
+#	Voici ses VI :
+#
+#	@menuParent : represente l'interface de menu parent, elle devra être affiché si on clique sur le bouton retour
+#	@builder : represente le fichier glade
+#	@object : represente l'interface de l'objet courent
+#	@btn_facile : selectionne le niveau de difficulté FACILE
+#	@btn_moyen : selectionne le niveau de difficulté MOYEN	
+#	@btn_difficile : selectionne le niveau de difficulté DIFFICILE
+#	@btn_retour : permet de revenir au menu parent
+#	@interfaceChoixGrille : represente l'interface qui devra être appelé au besoin
+
 class FenetreContreMontre < Fenetre
 
     attr_accessor :object
 
 	##
-	# initialize :
+	# initialize:
 	# 	Cette methode est le constructeur de la classe FenetreContreLaMontre, il permet de recuperer
 	#	le fichier glade et tout les objets qui le compose. Ensuite nous attribuons les bonnes 
 	#	actions a chaque objets récupérés.
@@ -44,7 +66,7 @@ class FenetreContreMontre < Fenetre
     end
 
     ##
-	# getObjet :
+	# getObjet:
 	# 	Cette methode permet d'envoyer sont objet (interface) a l'objet qui le demande.
 	#
 	# @return object qui represente l'interface de la fenetre du mode libre.
@@ -54,7 +76,7 @@ class FenetreContreMontre < Fenetre
 
 
 	##
-	# gestionSignaux :
+	# gestionSignaux:
 	#	Cette methode permet d'assigner des actions à chaques boutons récupérés dans le fichier galde.
 	def gestionSignaux
 
@@ -62,12 +84,12 @@ class FenetreContreMontre < Fenetre
 			@interfaceChoixGrille.difficulte=0
 			self.changerInterface(@interfaceChoixGrille.object, "Facile")
 		}
-		@btn_moyen.signal_connect("clicked"){
+		@btn_moyen.signal_connect("clicked"){print "\nTu as cliqué sur le mode Moyen"
 			@interfaceChoixGrille.difficulte=1
 			self.changerInterface(@interfaceChoixGrille.object, "Moyen")
 		}
 
-		@btn_difficile.signal_connect("clicked"){
+		@btn_difficile.signal_connect("clicked"){print "\nTu as cliqué sur le mode Difficile"
 			@interfaceChoixGrille.difficulte=2
 			self.changerInterface(@interfaceChoixGrille.object, "Difficile")
 		}
