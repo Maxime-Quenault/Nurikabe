4400bdf6b6bcc743625206f347cef933d5f6da71
Arthur Scherrer <arthurscherrer72@gmail.com>
2022-04-02 19:20:06 +0200

    doc Dossier Chrono

--- Chrono/Chronometre.rb
+++ Chrono/Chronometre.rb
@@ -1,20 +1,24 @@
-# Représente le Chrono du Nurikabe
+# La classe Chronometre :::
+#       - représente le système de chronomètre présent dans certains modes du jeu du Nurikabe.
+#       - est capable de faire accroître un chronomètre en partant de 0 (Contre-La-Montre)
+#       
+# Les VI de la classe sont :::
+#       - @temps    ==> temps restant ou temps en cours
+#       - @pause    ==> boolean indiquant si le jeu est en pause
 class Chronometre
 
     private_class_method :new
 
-    # temps restant ou temps en cours
-    @temps
-    # indiquant si le jeu est en pause
-    @pause
-
+    # Accès en lecture aux VI
     attr_reader :temps, :pause
 
+    #
     # Constructeur du Chronomètre
     def Chronometre.creer()
         new()
     end
 
+    #
     # initialize le chronomètre avec un temps à 0
     def initialize()
         @temps = 0
@@ -23,6 +27,7 @@ class Chronometre
         @debut = Time.new
     end 
 
+    #
     # méthode incrémentant le temps total du chronometre
     def top()
         if(!self.estEnPause?())
@@ -36,16 +41,19 @@ class Chronometre
         end
     end
 
+    #
     # renvoie si le temps est nul 
     def estNul?()
         return @temps == 0 
     end
 
+    #
     # renvoie si le jeu en pause
     def estEnPause?()
         return @pause == true
     end
 
+    #
     # actionne le chronometre
     def demarre()
         self.enlevePause()
@@ -53,23 +61,27 @@ class Chronometre
         top()
     end
 
+    #
     # met en pause le jeu
     def metEnPause()
         @pause = true 
         top()
     end
 
+    #
     # enleve la pause
     def enlevePause()
         @pause = false 
     end
 
+    #
     # retourne le temps sous la forme float
     def getTemps()
         top()
         return @temps
     end
 
+    #
     # augmente le temps par un malus
     def ajouteTemps(unMalus)
         @temps += unMalus 
